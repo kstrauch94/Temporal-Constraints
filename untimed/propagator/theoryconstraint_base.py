ad446085188c28b43499ec9d84ed251fdf07eed2
kstrauch94 <kstrauch0694@gmail.com>
2020-11-05 00:17:42 +0100

    memoization now uses atoms names instead of the tc class

--- untimed/propagator/theoryconstraint_base.py
+++ untimed/propagator/theoryconstraint_base.py
@@ -127,7 +127,7 @@ def reverse_assigned_time(info: atom_info, assigned_time: int) -> int:
 looked: Dict[Tuple[Any, int], Optional[List[int]]] = {}
 
 #@profile
-def form_nogood(tc, assigned_time: int) -> Optional[List[int]]:
+def form_nogood(t_atom_info, assigned_time: int) -> Optional[List[int]]:
 	"""
 	Forms a nogood based on the assigned time and atoms of a theory constraint
 
@@ -135,22 +135,23 @@ def form_nogood(tc, assigned_time: int) -> Optional[List[int]]:
 	:param assigned_time: the assigned time
 	:return: the nogood for the given assigned time and constraint
 	"""
-	if (tc, assigned_time) in looked:
-		return looked[tc, assigned_time]
+	tup = tuple(t_atom_info.keys())
+	if (tup, assigned_time) in looked:
+		return looked[tup, assigned_time]
 
 	ng: List[int] = []
 
 	try:
-		for uq_name, info in tc.t_atom_info.items():
-			time: int = reverse_assigned_time(info, assigned_time)
-			ng.append(TimeAtomToSolverLit.grab_lit(build_symbol_id(info, time)))
+		for uq_name in t_atom_info.keys():
+			time: int = reverse_assigned_time(t_atom_info[uq_name], assigned_time)
+			ng.append(TimeAtomToSolverLit.grab_lit(build_symbol_id(t_atom_info[uq_name], time)))
 	except KeyError:
 		# this error would happen if an id is not in the mapping
 		# if this happens it means the nogood does not exist for this assigned time
-		looked[tc, assigned_time] = None
+		looked[tup, assigned_time] = None
 		return None
 
-	looked[tc, assigned_time] = ng
+	looked[tup, assigned_time] = ng
 	return ng
 
 
@@ -343,7 +344,7 @@ class TheoryConstraint:
 
 	def check(self, control):
 		for assigned_time in range(self.min_time, self.max_time + 1):
-			ng = form_nogood(self, assigned_time)
+			ng = form_nogood(self.t_atom_info, assigned_time)
 			if check_assignment_complete(ng, control) == CONSTRAINT_CHECK["CONFLICT"]:
 				if not control.add_nogood(ng) or not control.propagate():
 					for lit in ng:
--- untimed/propagator/theoryconstraint_prop.py
+++ untimed/propagator/theoryconstraint_prop.py
@@ -30,7 +30,7 @@ class TheoryConstraintSize2Prop(TheoryConstraint):
 		"""
 		watches = set()
 		for assigned_time in range(self.min_time, self.max_time + 1):
-			lits = form_nogood(self, assigned_time)
+			lits = form_nogood(self.t_atom_info, assigned_time)
 			if lits is None:
 				continue
 			for lit in lits:
@@ -57,7 +57,7 @@ class TheoryConstraintSize2Prop(TheoryConstraint):
 				ats.update(get_at_from_name_id(name_id, self.t_atom_info))
 
 			for assigned_time in ats:
-				ng = form_nogood(self, assigned_time)
+				ng = form_nogood(self.t_atom_info, assigned_time)
 				if ng is None:
 					continue
 				if not control.add_nogood(ng) or not control.propagate():
@@ -79,7 +79,7 @@ class TheoryConstraintSize2Prop2WatchMap(TheoryConstraint):
 		"""
 		watches = set()
 		for assigned_time in range(self.min_time, self.max_time + 1):
-			lits = form_nogood(self, assigned_time)
+			lits = form_nogood(self.t_atom_info, assigned_time)
 			if lits is None:
 				continue
 			for lit in lits:
@@ -102,7 +102,7 @@ class TheoryConstraintSize2Prop2WatchMap(TheoryConstraint):
 
 			lit, assigned_time = change
 
-			ng = form_nogood(self, assigned_time)
+			ng = form_nogood(self.t_atom_info, assigned_time)
 			if ng is None:
 				return []
 			if not control.add_nogood(ng) or not control.propagate():
@@ -125,7 +125,7 @@ class TheoryConstraintNaiveProp(TheoryConstraint):
 		"""
 		watches = set()
 		for assigned_time in range(self.min_time, self.max_time + 1):
-			lits = form_nogood(self, assigned_time)
+			lits = form_nogood(self.t_atom_info, assigned_time)
 			if lits is None:
 				continue
 			for lit in lits:
@@ -152,7 +152,7 @@ class TheoryConstraintNaiveProp(TheoryConstraint):
 				ats.update(get_at_from_name_id(name_id, self.t_atom_info))
 
 			for assigned_time in ats:
-				ng = form_nogood(self, assigned_time)
+				ng = form_nogood(self.t_atom_info, assigned_time)
 				if ng is None:
 					continue
 
@@ -186,7 +186,7 @@ class TheoryConstraint2watchProp(TheoryConstraint):
 		"""
 		watches = set()
 		for assigned_time in range(self.min_time, self.max_time + 1):
-			lits = form_nogood(self, assigned_time)
+			lits = form_nogood(self.t_atom_info, assigned_time)
 			if lits is None:
 				continue
 			for lit in lits[:2]:
@@ -216,7 +216,7 @@ class TheoryConstraint2watchProp(TheoryConstraint):
 			replacement_info: List[List[int]] = []
 
 			for assigned_time in self.watches_to_at[change]:
-				ng = form_nogood(self, assigned_time)
+				ng = form_nogood(self.t_atom_info, assigned_time)
 				if ng is None:
 					continue
 
@@ -265,7 +265,7 @@ class TheoryConstraint2watchPropMap(TheoryConstraint):
 		"""
 		watches = set()
 		for assigned_time in range(self.min_time, self.max_time + 1):
-			lits = form_nogood(self, assigned_time)
+			lits = form_nogood(self.t_atom_info, assigned_time)
 			if lits is None:
 				continue
 			for lit in lits[:2]:
@@ -290,7 +290,7 @@ class TheoryConstraint2watchPropMap(TheoryConstraint):
 			delete_add = []
 
 			lit, assigned_time = change
-			ng = form_nogood(self, assigned_time)
+			ng = form_nogood(self.t_atom_info, assigned_time)
 			if ng is None:
 				return []
 
@@ -319,7 +319,7 @@ class TheoryConstraintSize2TimedProp(TheoryConstraint):
 		add all solver literals for the constraint as watches
 		"""
 		for assigned_time in range(self.min_time, self.max_time + 1):
-			lits = form_nogood(self, assigned_time)
+			lits = form_nogood(self.t_atom_info, assigned_time)
 			if lits is None:
 				continue
 			for lit in lits:
@@ -339,7 +339,7 @@ class TheoryConstraintSize2TimedProp(TheoryConstraint):
 		ats = get_at_from_name_id(change, self.t_atom_info)
 
 		for assigned_time in ats:
-			ng = form_nogood(self, assigned_time)
+			ng = form_nogood(self.t_atom_info, assigned_time)
 			if ng is None:
 				continue
 
@@ -361,7 +361,7 @@ class TheoryConstraintTimedProp(TheoryConstraint):
 		add all solver literals for the constraint as watches
 		"""
 		for assigned_time in range(self.min_time, self.max_time + 1):
-			lits = form_nogood(self, assigned_time)
+			lits = form_nogood(self.t_atom_info, assigned_time)
 			if lits is None:
 				continue
 			for lit in lits:
@@ -378,7 +378,7 @@ class TheoryConstraintTimedProp(TheoryConstraint):
 		ats = get_at_from_name_id(change, self.t_atom_info)
 
 		for assigned_time in ats:
-			ng = form_nogood(self, assigned_time)
+			ng = form_nogood(self.t_atom_info, assigned_time)
 			if ng is None:
 				continue
 
